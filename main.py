f3c95069acd01a1a2c27dd8cbc4f447601c5bdf2
MaGo1024 <olumideodusote@gmail.com>
2026-02-15 01:43:39 +0000

    Detecting car collision with player.

--- main.py
+++ main.py
@@ -22,3 +22,11 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
+    # Detect collision with any car
+    for car in car_manager.all_cars:
+        if car.distance(player_1) < 20:
+            game_is_on = False
+
+
+screen.exitonclick()
+
